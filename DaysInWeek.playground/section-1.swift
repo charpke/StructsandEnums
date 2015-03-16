34ba1bcd444f4cce59237469b762be75cb5485e6
charles harpke <charpke@charles-harpkes-MacBook-Pro.local>
2015-03-15 21:24:03 -0500

    initial commit

---
+++ DaysInWeek.playground/section-1.swift
@@ -0,0 +1,25 @@
+//DaysInWeek
+
+import UIKit
+
+
+enum Day {
+    case Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+func weekdayOrWeekend(dayOfWeek: Day) -> String {
+
+    switch dayOfWeek {
+    case .Monday, .Tuesday, .Wednesday, .Thursday, .Friday:
+            return "It's a weekday"
+    case .Saturday, .Sunday:
+        return "Yay! It's the weekend!"
+        default:
+        return "Not a valid day"
+    }
+}
+var today = Day.Monday
+today = Day.Sunday
+
+weekdayOrWeekend(Day.Sunday)
+
